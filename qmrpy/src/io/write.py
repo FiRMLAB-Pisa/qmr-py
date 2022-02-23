a976605ab91acdca8b4736a58e07bdbcc523b447
Matteo Cencini <mcencini@imago7.local>
2022-02-23 16:35:23 +0100

    Trying to fix dicom

--- qmrpy/src/io/write.py
+++ qmrpy/src/io/write.py
@@ -87,7 +87,6 @@ def write_dicom(image: np.ndarray, info: Dict, series_description: str, outpath:
         except:
             pass
         
-        dsets[n].EchoNumber = '0'
         dsets[n][0x0018, 0x0086].value = '1' # Echo Number
         dsets[n].InversionTime = '0'
         dsets[n].EchoTime = '0'
